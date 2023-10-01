8ce9e510447a9a4939117ea776474f22bdb2df04
Anastasiia Kholod <121211989+asyaruda@users.noreply.github.com>
2023-10-01 18:48:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { Counter } from './features/Counter'
 import { Waiter } from './features/waiters'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-       {<Counter />}
       <Waiter />
   </Provider>
 );
